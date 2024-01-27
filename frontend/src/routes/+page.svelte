a8aa62cb7e61f3f01587b9f5e516535eb9c52205
chefsApprentice <cyberclanofficialacount@gmail.com>
2024-01-27 11:03:41 +0000

    added result file

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import RedText from "$lib/RedText.svelte";
+    import Result from "$lib/Result.svelte";
 
     console.log("Royal Hackaway");
 
