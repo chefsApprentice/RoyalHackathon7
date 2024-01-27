9a5433a95f18f0237b25457975c943d6ecab98e9
Zolo101 <umadeguy@gmail.com>
2024-01-27 10:56:06 +0000

    Video

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+
     console.log("Royal Hackaway");
+
+    let videoElement: HTMLVideoElement;
+
+    onMount(() => {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(stream => {
+                const video = document.querySelector("video");
+                videoElement.srcObject = stream;
+                videoElement.onloadedmetadata = () => videoElement.play();
+            })
+            .catch(err => console.error(err));
+    })
 </script>
 
 <div>
@@ -10,6 +24,8 @@
     <p>Royal Hackaway</p>
     <p>Royal Hackaway</p>
 </div>
+
+<video bind:this={videoElement}/>
 
 <style>
     p {
